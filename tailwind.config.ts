9d9b9eecc19ccd23f875c13c5aa8b72882fab9ac
Zarni Hlawn <zarnihlawnoff@gmail.com>
2024-09-03 19:58:51 +0630

    add_banner_&&_styling_backgroundSpanTitle_&&_tailwindconfig

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,4 @@
+import { transform } from "next/dist/build/swc";
 import type { Config } from "tailwindcss";
 
 const config: Config = {
@@ -8,22 +9,30 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      keyframes: {
+        wiggle: {
+          '0%, 100%': { transform: 'rotate(0deg)' },
+          '30%': {transform: 'rotate(20deg)'}
+        }
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "linear-gradient-yellow-to-orange": "linear-gradient(to right, #FFE89C, #FFB28C)",
       },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
         background: "hsl(var(--background))",
+        backgroundBlue: "hsl(var(--background-blue))",
         foreground: "hsl(var(--foreground))",
         primary: "var(--primary)",
         midGrey: "var(--mid-grey-color)",
         darkGrey: "var(--dark-grey-color)",
         bg1: "var(--bg-1)",
-        neutralLightest: "var(--neutral-lightest)",
+        neutralLightest: "var(--neutral-lightest)"
       },
     },
     fontFamily: {
