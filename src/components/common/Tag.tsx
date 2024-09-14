4e72ef659bf47d4aaff91978b60350496d1804a6
Pyae Sone Myo <66440980+Rickaym@users.noreply.github.com>
2024-09-14 15:37:25 +0630

    feat: support different variants

--- src/components/common/Tag.tsx
+++ src/components/common/Tag.tsx
@@ -1,6 +1,21 @@
-const Tag = ({ tag, clickable }: { tag: string; clickable?: boolean }) => {
+import { cn } from "@/lib/utils";
+import { MouseEventHandler } from "react";
+
+type TagProps = {
+  tag: string;
+  clickable?: boolean;
+  variant?: "primary" | "outline";
+  onClick?: MouseEventHandler<HTMLDivElement>
+};
+
+const variants = {
+  primary: "rounded-[20px] bg-neutralLightest px-2 py-1",
+  outline: "rounded-[35px] border border-black px-4 py-2",
+};
+
+const Tag = ({ tag, clickable, variant, onClick }: TagProps) => {
   return (
-    <div className="flex gap-2 items-center rounded-[20px] bg-neutralLightest px-2 py-1">
+    <div onClick={onClick} className={cn("flex items-center gap-2", variants[variant || "primary"])}>
       <p className="c-small">{tag}</p>
       {clickable && (
         <svg
