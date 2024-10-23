d54b7107e388d2f5b951133ba7050c55ee24b141
Sai Seng <saisengmongli14@gmail.com>
2024-10-24 00:07:21 +0700

    chore: restyle home page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,9 +1,10 @@
-import Testimonial from "@/components/common/Testimonial";
+import Testimonial from "@/components/main/Testimonial";
 import Tutorial from "@/components/main/Tutorial";
 import LatestNews from "@/components/main/LatestNews";
 import HomeSection from "@/components/main/HomeSection";
 import HotTrend from "@/components/main/HotTrend";
 import FAQ from "@/components/main/FAQ";
+import Team from "@/components/main/Team";
 
 export default function Home() {
   const lang = "en";
@@ -14,6 +15,7 @@ export default function Home() {
       <Testimonial />
       <Tutorial />
       <LatestNews />
+      <Team />
       <FAQ />
     </div>
   );
