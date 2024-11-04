05bd1cf720a9e17a10ac883db359cc1fb0346476
Pyae Sone Myo <66440980+Rickaym@users.noreply.github.com>
2024-11-04 19:24:23 +0630

    fix: no need for async function

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -3,7 +3,7 @@ import { redirect } from "next/navigation";
 import CreateForm from "@/components/main/CreateForm";
 import { useCurrentUser } from "@/hooks/useCurrentUser";
 
-export default async function CreatePage() {
+export default function CreatePage() {
   const user = useCurrentUser();
 
   if (!user) redirect("/");
