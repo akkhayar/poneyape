fc9d7c98acd166a0793de70f58e8ccd31511eaf5
Pyae Sone Myo <66440980+Rickaym@users.noreply.github.com>
2024-11-04 16:52:10 +0630

    feat: combine firebase files

--- src/lib/firebase/firebase.ts
+++ src/lib/firebase/firebase.ts
@@ -1,8 +1,22 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { FirebaseError, initializeApp } from "firebase/app";
+import {
+  Auth,
+  createUserWithEmailAndPassword,
+  FacebookAuthProvider,
+  getAuth,
+  GithubAuthProvider,
+  GoogleAuthProvider,
+  sendEmailVerification,
+  sendPasswordResetEmail,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+} from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
+
+import { APIResponse } from "@/types";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -19,7 +33,145 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-export const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
+
+export class FirebaseClient {
+  private auth: Auth;
+
+  constructor(auth: Auth) {
+    this.auth = auth;
+  }
+
+  async signInWithGoogle() {
+    try {
+      const provider = new GoogleAuthProvider();
+      provider.setCustomParameters({ prompt: "select_account" });
+      await signInWithPopup(auth, provider);
+      return true;
+    } catch (error) {
+      console.log("FirebaseClient loginWithGoogle error", error);
+      return false;
+    }
+  }
+
+  async signInWithFacebook() {
+    const provider = new FacebookAuthProvider();
+
+    try {
+      const userCreds = await signInWithPopup(auth, provider);
+      const idToken = await userCreds.user.getIdToken();
+
+      const response = await fetch("/api/auth/sign-in", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ idToken }),
+      });
+
+      const resBody = (await response.json()) as unknown as APIResponse<string>;
+      if (response.ok && resBody.success) {
+        return true;
+      } else return false;
+    } catch (error) {
+      console.error("Error signing in with Google", error);
+      if (
+        error instanceof FirebaseError &&
+        error.code === "auth/account-exists-with-different-credential"
+      ) {
+        return "Account already exist with different provider. Try logging in with other provider and link your account";
+      }
+      return false;
+    }
+  }
+
+  async signInWithGithub() {
+    const provider = new GithubAuthProvider();
+
+    try {
+      const userCreds = await signInWithPopup(auth, provider);
+      const idToken = await userCreds.user.getIdToken();
+      const response = await fetch("/api/auth/sign-in", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ idToken }),
+      });
+
+      const resBody = (await response.json()) as unknown as APIResponse<string>;
+      if (response.ok && resBody.success) {
+        return true;
+      } else return false;
+    } catch (error) {
+      console.error("Error signing in with Github", error);
+      if (
+        error instanceof FirebaseError &&
+        error.code === "auth/account-exists-with-different-credential"
+      ) {
+        return "Account already exist with different provider. Try logging in with other provider and link your account";
+      }
+      return false;
+    }
+  }
+
+  async signInWithEmail(email: string, password: string) {
+    try {
+      const userCreds = await signInWithEmailAndPassword(auth, email, password);
+      const idToken = await userCreds.user.getIdToken();
+
+      const response = await fetch("/api/auth/sign-in", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ idToken }),
+      });
+
+      const resBody = (await response.json()) as unknown as APIResponse<string>;
+      if (response.ok && resBody.success) {
+        return true;
+      } else return false;
+    } catch (error) {
+      console.error("Error signing in with Google", error);
+      if (
+        error instanceof FirebaseError &&
+        error.code === "auth/account-exists-with-different-credential"
+      ) {
+        return "Account already exist with different provider. Try logging in with other provider and link your account";
+      }
+      return false;
+    }
+  }
+
+  async signUpWithEmail(email: string, password: string) {
+    try {
+      const userCreds = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password,
+      );
+      await sendEmailVerification(userCreds.user);
+      await userCreds.user.getIdToken();
+    } catch (error) {
+      console.error("Error signing in with Google", error);
+      return false;
+    }
+  }
+
+  async resetPassword(email: string) {
+    try {
+      const response = await sendPasswordResetEmail(auth, email);
+
+      return true;
+    } catch (error) {
+      console.error("Error resetting link", error);
+      return false;
+    }
+  }
+}
+
+export const firebaseClient = new FirebaseClient(auth);
