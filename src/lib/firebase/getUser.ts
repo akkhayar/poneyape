e600c684f22c8602ac948f3c089292890cdfeb07
Pyae Sone Myo <66440980+Rickaym@users.noreply.github.com>
2024-11-04 16:51:58 +0630

    feat: remove firebase admin endpoints

--- src/lib/firebase/getUser.ts
+++
@@ -1,8 +0,0 @@
-"use server";
-
-import { auth as adminAuth } from "../firebase/firebase-admin";
-
-export const getUserById = async (id: string) => {
-  const user = await adminAuth.getUser(id);
-  return user;
-};
