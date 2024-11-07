a3f52276fb3f3ebe401686e54212eee0e39a0f51
Pyae Sone Myo <66440980+Rickaym@users.noreply.github.com>
2024-11-08 00:28:43 +0630

    fix: remove unused import

--- src/components/common/CarouselSlider.tsx
+++ src/components/common/CarouselSlider.tsx
@@ -11,7 +11,6 @@ import {
   CarouselContent,
   CarouselItem,
 } from "@/components/ui/carousel";
-import { useFirestoreUser } from "@/hooks/useFirestoreUser";
 import { cn } from "@/lib/utils";
 import { WebsiteDataFetch } from "@/types";
 
